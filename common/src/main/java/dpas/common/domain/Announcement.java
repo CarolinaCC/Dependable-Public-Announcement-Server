d2054e56bb6a2a2ecbdfba39decdbc75e8d3bc16
CatarinaPedreira <catarina.pedreira@tecnico.ulisboa.pt>
2020-03-13 12:32:08 +0000

    Added actual Announcement Test

--- common/src/main/java/dpas/common/domain/Announcement.java
+++ common/src/main/java/dpas/common/domain/Announcement.java
@@ -3,32 +3,28 @@ package dpas.common.domain;
 import dpas.common.domain.exception.*;
 
 import java.security.*;
-import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Date;
 
 public class Announcement {
     private byte[] _signature;
     private User _user;
     private String _message;
     private ArrayList<Announcement> _references; // Can be null
-    private LocalDate _publishTime; // Date and time of the post
 
-    public Announcement(byte[] signature, User user, String message, ArrayList<Announcement> references, LocalDate publishTime) throws NullSignatureException, NullMessageException,
-            NullPublishTimeException, NullPostException, InvalidSignatureException, NoSuchAlgorithmException, InvalidKeyException, SignatureException,
+    public Announcement(byte[] signature, User user, String message, ArrayList<Announcement> references) throws NullSignatureException, NullMessageException,
+             NullPostException, InvalidSignatureException, NoSuchAlgorithmException, InvalidKeyException, SignatureException,
             NullUserException {
 
-        checkArguments(signature, user, message, publishTime, references);
+        checkArguments(signature, user, message, references);
         checkSignature(signature, user, message);
         this._message = message;
         this._signature = signature;
         this._user = user;
         this._references = references;
-        this._publishTime = publishTime;
     }
 
-    public void checkArguments(byte[] signature, User user, String message, LocalDate publishTime, ArrayList<Announcement> references) throws NullSignatureException,
-            NullMessageException, NullPublishTimeException, NullPostException, NullUserException {
+    public void checkArguments(byte[] signature, User user, String message, ArrayList<Announcement> references) throws NullSignatureException,
+            NullMessageException, NullPostException, NullUserException {
 
         if (signature == null) {
             throw new NullSignatureException();
@@ -39,9 +35,6 @@ public class Announcement {
         if (message == null) {
             throw new NullMessageException();
         }
-        if (publishTime == null) {
-            throw new NullPublishTimeException();
-        }
 
         if (references != null) {
             if (references.contains(null)) {
@@ -51,7 +44,7 @@ public class Announcement {
     }
 
     public void checkSignature(byte[] signature, User user, String message) throws InvalidSignatureException, InvalidKeyException, NoSuchAlgorithmException,
-            SignatureException, NullMessageException {
+            SignatureException {
 
         byte[] messageBytes = message.getBytes();
         PublicKey publicKey = user.getPublicKey();
@@ -79,11 +72,9 @@ public class Announcement {
         return this._references;
     }
 
-    public LocalDate getPublishTime() { return this._publishTime; }
-
-    //public String printPublishTime() { return this._publishTime.toString(); }
-
-    public User getUser() { return this._user; }
+    public User getUser() {
+        return this._user;
+    }
 
     public String getPostUsername() {
         return this._user.getUsername();
--- common/src/test/java/dpas/common/domain/AnnouncementTest.java
+++ common/src/test/java/dpas/common/domain/AnnouncementTest.java
@@ -1,2 +1,124 @@
+package dpas.common.domain;
+
+import dpas.common.domain.exception.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.security.*;
+import java.sql.Array;
+import java.sql.Ref;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Date;
+
+import static org.junit.Assert.assertEquals;
+
 public class AnnouncementTest {
+
+    private static final String MESSAGE = "Hello World";
+    private static final String OTHER_MESSAGE = "This is another announcement";
+    private static final byte[] MESSAGE_BYTES = MESSAGE.getBytes();
+
+    private static final String USERNAME = "Einstein";
+    private static final String OTHER_USERNAME = "Marie Curie";
+
+    private ArrayList<Announcement> _references = new ArrayList<>();
+    private byte[] _signature;
+    private User _user;
+
+    @Before
+    public void setup() throws NoSuchAlgorithmException, InvalidKeyException, SignatureException, NullPublicKeyException, NullUsernameException, NullMessageException,
+            NullSignatureException, NullUserException, NullPostException, InvalidSignatureException {
+
+        //Generate public key
+        KeyPairGenerator keygen = KeyPairGenerator.getInstance("RSA");
+        keygen.initialize(2048);
+        KeyPair keyPair = keygen.generateKeyPair();
+        PrivateKey privateKey = keyPair.getPrivate();
+        PublicKey publicKey = keyPair.getPublic();
+
+        //Generate valid signature
+        Signature sign = Signature.getInstance("SHA256withRSA");
+        sign.initSign(privateKey);
+        sign.update(MESSAGE_BYTES);
+        this._signature = sign.sign();
+
+        //Generate user
+        this._user = new User(USERNAME, publicKey);
+
+        //Create another announcement
+        KeyPairGenerator otherKeyGen = KeyPairGenerator.getInstance("RSA");
+        keygen.initialize(2048);
+        KeyPair otherKeyPair = otherKeyGen.generateKeyPair();
+        PublicKey otherPublicKey = otherKeyPair.getPublic();
+        PrivateKey otherPrivateKey = otherKeyPair.getPrivate();
+
+        sign.initSign(otherPrivateKey);
+        sign.update(OTHER_MESSAGE.getBytes());
+        byte[] otherSignature = sign.sign();
+
+        User otherUser = new User(OTHER_USERNAME, otherPublicKey);
+        Announcement ref = new Announcement(otherSignature, otherUser, OTHER_MESSAGE, null);
+
+        //Add it to references
+        _references.add(ref);
+    }
+
+    @After
+    public void tearDown() {
+
+    }
+
+    @Test
+    public void validAnnouncement() throws InvalidKeyException, NullMessageException, NoSuchAlgorithmException,
+            InvalidSignatureException, NullSignatureException, NullUserException, SignatureException, NullPostException {
+
+        Announcement announcement = new Announcement(_signature, _user, MESSAGE, _references);
+        assertEquals(announcement.getSignature(), _signature);
+        assertEquals(announcement.getUser(), _user);
+        assertEquals(announcement.getMessage(), MESSAGE);
+        assertEquals(announcement.getReferences(), _references);
+    }
+
+    @Test(expected = NullSignatureException.class)
+    public void nullSignature() throws InvalidKeyException, NullMessageException, NoSuchAlgorithmException,
+            InvalidSignatureException, NullSignatureException, NullUserException, SignatureException, NullPostException {
+
+        Announcement announcement = new Announcement(null, _user, MESSAGE, _references);
+    }
+
+
+    @Test(expected = NullUserException.class)
+    public void nullUser() throws InvalidKeyException, NullMessageException, NoSuchAlgorithmException,
+            InvalidSignatureException, NullSignatureException, NullUserException, SignatureException, NullPostException {
+
+        Announcement announcement = new Announcement(_signature, null, MESSAGE, _references);
+    }
+
+    @Test(expected = NullMessageException.class)
+    public void nullMessage() throws InvalidKeyException, NullMessageException, NoSuchAlgorithmException,
+            InvalidSignatureException, NullSignatureException, NullUserException, SignatureException, NullPostException {
+
+        Announcement announcement = new Announcement(_signature, _user, null, _references);
+    }
+
+    @Test(expected = NullPostException.class)
+    public void nullReferences() throws InvalidKeyException, NullMessageException, NoSuchAlgorithmException,
+            InvalidSignatureException, NullSignatureException, NullUserException, SignatureException, NullPostException {
+
+        ArrayList<Announcement> refNullElement = new ArrayList<>();
+        refNullElement.add(null);
+
+        Announcement announcement = new Announcement(_signature, _user, MESSAGE, refNullElement);
+    }
+
+    @Test(expected = InvalidSignatureException.class)
+    public void invalidSignature() throws InvalidKeyException, NullMessageException, NoSuchAlgorithmException,
+            InvalidSignatureException, NullSignatureException, NullUserException, SignatureException, NullPostException {
+
+        byte[] invalidSig = "InvalidSignature".getBytes();
+        Announcement announcement = new Announcement(invalidSig, _user, MESSAGE, _references);
+    }
+
 }
