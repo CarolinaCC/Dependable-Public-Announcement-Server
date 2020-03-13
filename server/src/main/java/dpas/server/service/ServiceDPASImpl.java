ff9dcff7d749ce1ac78c2552c8a2818d373ef9f9
Miguel Barros <miguel.v.barros@tecnico.ulisboa.pt>
2020-03-13 18:38:00 +0000

    Added Server Service Implementation Class

--- server/src/main/java/dpas/server/service/ServiceDPASImpl.java
+++ server/src/main/java/dpas/server/service/ServiceDPASImpl.java
@@ -1,6 +1,32 @@
 package dpas.server.service;
 
+import dpas.grpc.contract.Contract;
 import dpas.grpc.contract.ServiceDPASGrpc;
+import dpas.grpc.contract.Contract.RegisterRequest;
+import dpas.grpc.contract.Contract.RegisterReply;
+import io.grpc.stub.StreamObserver;
 
 public class ServiceDPASImpl extends ServiceDPASGrpc.ServiceDPASImplBase {
+
+    @Override
+    public void register(RegisterRequest request, StreamObserver<RegisterReply> replyObserver) {
+
+    }
+
+    @Override
+    public void post(Contract.PostRequest request, StreamObserver<Contract.PostReply> responseObserver) {
+
+    }
+
+    @Override
+    public void postGeneral(Contract.PostRequest request, StreamObserver<Contract.PostReply> responseObserver) {
+    }
+
+    @Override
+    public void read(Contract.ReadRequest request, StreamObserver<Contract.ReadReply> responseObserver) {
+    }
+
+    @Override
+    public void readGeneral(Contract.ReadRequest request, StreamObserver<Contract.ReadReply> responseObserver) {
+    }
 }
