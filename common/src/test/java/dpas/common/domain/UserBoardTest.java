5010a0035d92c15f1a4d845e0bad06e4e0c80d5d
CarolinaCC <carolzocas@gmail.com>
2020-03-13 22:12:29 +0000

    Add UserBoard Test

--- common/src/test/java/dpas/common/domain/UserBoardTest.java
+++ common/src/test/java/dpas/common/domain/UserBoardTest.java
@@ -1,6 +1,7 @@
 package dpas.common.domain;
 
 import static org.junit.Assert.assertEquals;
+
 import dpas.common.domain.exception.*;
 import org.junit.After;
 import org.junit.Before;
@@ -8,13 +9,12 @@ import org.junit.Test;
 
 import java.io.UnsupportedEncodingException;
 import java.security.*;
+import java.util.ArrayList;
 
 public class UserBoardTest {
 
-    private Announcement _announcementA;
-    private Announcement _announcementB;
-    private User _userA;
-    private User _userB;
+    private Announcement _announcementValid;
+    private Announcement _announcementInvalid;
     private UserBoard _userBoard;
 
     @Before
@@ -24,41 +24,69 @@ public class UserBoardTest {
         keygen.initialize(1024);
         KeyPair keyPair = keygen.generateKeyPair();
         PublicKey publicKey = keyPair.getPublic();
-        _userA = new User("FIRST_USER_NAME", publicKey);
+        User user = new User("FIRST_USER_NAME", publicKey);
         //Generate valid signature
         Signature sign = Signature.getInstance("SHA256withRSA");
         sign.initSign(keyPair.getPrivate());
         sign.update("MESSAGE".getBytes());
         byte[] signature = sign.sign();
         // Generate Announcement A
-        _announcementA = new Announcement(signature, _userA, "MESSAGE", null);
+        _announcementValid = new Announcement(signature, user, "MESSAGE", null);
 
+        // Get UserBoard
+        _userBoard = user.getUserBoard();
 
         // generate user B
         keygen = KeyPairGenerator.getInstance("RSA");
         keygen.initialize(1024);
         keyPair = keygen.generateKeyPair();
         publicKey = keyPair.getPublic();
-        _userB = new User("FIRST_USER_NAME", publicKey);
+        user = new User("FIRST_USER_NAME", publicKey);
         //Generate valid signature
         sign = Signature.getInstance("SHA256withRSA");
         sign.initSign(keyPair.getPrivate());
         sign.update("MESSAGE".getBytes());
         signature = sign.sign();
         // Generate Announcement B
-        _announcementB = new Announcement(signature, _userB, "MESSAGE", null);
-
-        // Generate Board
-        _userBoard = new UserBoard(_userA);
+        _announcementInvalid = new Announcement(signature, user, "MESSAGE", null);
     }
 
     @After
     public void tearDown() {
     }
 
+    @Test
+    public void validPost() throws InvalidNumberOfPostsException, InvalidUserException, NullAnnouncementException {
+        _userBoard.post(_announcementValid);
+        assertEquals(_userBoard.read(1).get(0), _announcementValid);
+    }
 
 
+    @Test(expected = NullUserException.class)
+    public void nullAnnouncementPost() throws InvalidUserException, NullAnnouncementException {
+        _userBoard.post(null);
+    }
 
+    @Test(expected = InvalidUserException.class)
+    public void invalidUserPost() throws InvalidUserException, NullAnnouncementException {
+        _userBoard.post(_announcementInvalid);
+    }
+
+    @Test
+    public void validRead() throws NullAnnouncementException, InvalidNumberOfPostsException, InvalidUserException {
+        _userBoard.post(_announcementValid);
+        _userBoard.post(_announcementValid);
+        ArrayList<Announcement> expectedAnnouncements = new ArrayList<Announcement>();
+        expectedAnnouncements.add(_announcementValid);
+        expectedAnnouncements.add(_announcementValid);
+        assertEquals(_userBoard.read(2), expectedAnnouncements);
+    }
+
+    @Test(expected = InvalidNumberOfPostsException.class)
+    public void invalidNumberOfPostsRead() throws NullAnnouncementException, InvalidNumberOfPostsException, InvalidUserException {
+        _userBoard.post(_announcementValid);
+        _userBoard.read(-1);
+    }
 
 
 }
