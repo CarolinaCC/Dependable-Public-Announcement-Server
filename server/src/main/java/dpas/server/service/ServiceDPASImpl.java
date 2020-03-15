43a8a688c5066b021c55b8253e4f642681cee894
Miguel Barros <miguel.v.barros@tecnico.ulisboa.pt>
2020-03-15 16:30:24 +0000

    Code cleanup

--- server/src/main/java/dpas/server/service/ServiceDPASImpl.java
+++ server/src/main/java/dpas/server/service/ServiceDPASImpl.java
@@ -10,6 +10,7 @@ import dpas.common.domain.exception.NullUsernameException;
 import dpas.common.domain.exception.UserAlreadyExistsException;
 import dpas.common.domain.UserBoard;
 import dpas.grpc.contract.Contract;
+import dpas.grpc.contract.Contract.RegisterReplyOrBuilder;
 import dpas.grpc.contract.ServiceDPASGrpc;
 import dpas.grpc.contract.Contract.RegisterRequest;
 import dpas.grpc.contract.Contract.RegisterReply;
@@ -24,6 +25,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 
+import static dpas.grpc.contract.Contract.RegisterStatus.*;
+
 public class ServiceDPASImpl extends ServiceDPASGrpc.ServiceDPASImplBase {
 
     private ConcurrentHashMap<PublicKey, User> _users = new ConcurrentHashMap<>();
@@ -31,41 +34,29 @@ public class ServiceDPASImpl extends ServiceDPASGrpc.ServiceDPASImplBase {
 
     @Override
     public void register(RegisterRequest request, StreamObserver<RegisterReply> replyObserver) {
+        Contract.RegisterStatus replyStatus = REGISTERSTATUS_OK;
         try {
             //Get Public Key From byte[]
             PublicKey key = KeyFactory.getInstance("RSA")
-                    .generatePublic(new X509EncodedKeySpec(request.getPublicKey().toByteArray()));
+                            .generatePublic(new X509EncodedKeySpec(request.getPublicKey().toByteArray()));
             String username = request.getUsername();
-
             User user = new User(username, key);
 
-
             User curr = _users.putIfAbsent(key, user);
-
             if (curr != null) {
                 //User with public key already exists
-                replyObserver.onNext(RegisterReply.newBuilder()
-                        .setStatus(Contract.RegisterStatus.REGISTERSTATUS_REPEATED_USER)
-                        .build());
-            } else {
-                replyObserver.onNext(RegisterReply.newBuilder()
-                        .setStatus(Contract.RegisterStatus.REGISTERSTATUS_OK)
-                        .build());
+                replyStatus = REGISTERSTATUS_REPEATED_USER;
             }
-        } catch (NullPublicKeyException | InvalidKeySpecException | NoSuchAlgorithmException e) {
-            replyObserver.onNext(RegisterReply.newBuilder()
-                    .setStatus(Contract.RegisterStatus.REGISTERSTATUS_NULL_PUBLICKEY)
-                    .build());
-
-        } catch (NullUsernameException e) {
-            replyObserver.onNext(RegisterReply.newBuilder()
-                    .setStatus(Contract.RegisterStatus.REGISTERSTATUS_NULL_USERNAME)
-                    .build());
-
-        } catch (NullUserException e) {
-            e.printStackTrace();
-            //Should Never Happen
         }
+        catch (NullPublicKeyException | InvalidKeySpecException | NoSuchAlgorithmException e) {
+            replyStatus = REGISTERSTATUS_NULL_PUBLICKEY;
+        } catch (NullUsernameException e) {
+            replyStatus = REGISTERSTATUS_NULL_USERNAME;
+        } catch (NullUserException e) {
+            //Should Never Happen
+            e.printStackTrace();
+        }
+        replyObserver.onNext(RegisterReply.newBuilder().setStatus(replyStatus).build());
         replyObserver.onCompleted();
 
     }
