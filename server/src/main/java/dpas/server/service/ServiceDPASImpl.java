057a55033791837e9b677f8ba7ce1f05e0e9da78
Miguel Barros <miguel.v.barros@tecnico.ulisboa.pt>
2020-03-15 14:55:18 +0000

    Added User and General Boards to Server

--- server/src/main/java/dpas/server/service/ServiceDPASImpl.java
+++ server/src/main/java/dpas/server/service/ServiceDPASImpl.java
@@ -1,12 +1,20 @@
 package dpas.server.service;
 
+import dpas.common.domain.GeneralBoard;
+import dpas.common.domain.User;
 import dpas.grpc.contract.Contract;
 import dpas.grpc.contract.ServiceDPASGrpc;
 import dpas.grpc.contract.Contract.RegisterRequest;
 import dpas.grpc.contract.Contract.RegisterReply;
 import io.grpc.stub.StreamObserver;
 
+import java.security.PublicKey;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class ServiceDPASImpl extends ServiceDPASGrpc.ServiceDPASImplBase {
+
+    private ConcurrentHashMap<PublicKey, User> _users = new ConcurrentHashMap<>();
+    private GeneralBoard _generalBoard = new GeneralBoard();
 
     @Override
     public void register(RegisterRequest request, StreamObserver<RegisterReply> replyObserver) {
