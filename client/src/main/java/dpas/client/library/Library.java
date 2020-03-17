36bee962b3259e1505b8450a57ec6297197cee71
Miguel Barros <miguel.v.barros@tecnico.ulisboa.pt>
2020-03-17 18:18:28 +0000

    Added Library Constructor

--- client/src/main/java/dpas/client/library/Library.java
+++ client/src/main/java/dpas/client/library/Library.java
@@ -2,12 +2,18 @@ package dpas.client.library;
 
 import dpas.common.domain.Announcement;
 import dpas.grpc.contract.ServiceDPASGrpc;
+import io.grpc.netty.shaded.io.grpc.netty.NettyChannelBuilder;
 
 import java.security.PublicKey;
 
 public class Library {
 
-    public ServiceDPASGrpc.ServiceDPASStub _stub;
+    public ServiceDPASGrpc.ServiceDPASBlockingStub _stub;
+
+    public Library(String host, int port) {
+        var _channel = NettyChannelBuilder.forAddress(host, port).usePlaintext().build();
+        _stub = ServiceDPASGrpc.newBlockingStub(_channel);
+    }
 
     public void register(PublicKey publicKey, String username) {
 
