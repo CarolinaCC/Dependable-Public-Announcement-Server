b6fc6d7e1b8929218a515cfba738f4c51be909de
Miguel Barros <miguel.v.barros@tecnico.ulisboa.pt>
2020-03-17 18:06:06 +0000

    Client Class

---
+++ client/src/main/java/dpas/client/library/Library.java
@@ -0,0 +1,28 @@
+package dpas.client.library;
+
+import dpas.common.domain.Announcement;
+
+import java.security.PublicKey;
+
+public class Library {
+
+    public void register(PublicKey publicKey, String username) {
+
+    }
+
+    public void post(PublicKey key, char[] message, Announcement[] a) {
+
+    }
+
+    public void postGeneral(PublicKey key, char[] message, Announcement[] a) {
+
+    }
+
+    public void read(PublicKey publicKey) {
+
+    }
+
+    public void readGeneral(int number) {
+
+    }
+}
