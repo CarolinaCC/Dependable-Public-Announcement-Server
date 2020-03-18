7169bed146b8b059425168c3c31d10b03a8d4cd4
Miguel Barros <miguel.v.barros@tecnico.ulisboa.pt>
2020-03-18 14:39:43 +0000

    Persistence Manager constructor

--- server/src/main/java/dpas/server/persistence/PersistenceManager.java
+++ server/src/main/java/dpas/server/persistence/PersistenceManager.java
@@ -4,7 +4,14 @@ import dpas.common.domain.Announcement;
 import dpas.common.domain.GeneralBoard;
 import dpas.common.domain.User;
 import dpas.server.service.ServiceDPASImpl;
+import org.apache.commons.io.FileUtils;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.security.PublicKey;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -13,13 +20,22 @@ public class PersistenceManager {
     private ConcurrentHashMap<String, Announcement> _announcements;
     private ConcurrentHashMap<PublicKey, User> _users;
     private GeneralBoard _generalBoard;
-    private String path;
+    private File _file;
+    public PersistenceManager(String path) throws IOException {
+        _announcements = new ConcurrentHashMap<>();
+        _users = new ConcurrentHashMap<>();
+        _generalBoard = new GeneralBoard();
 
-    public PersistenceManager(ConcurrentHashMap<String, Announcement> announcements, ConcurrentHashMap<PublicKey, User> users, GeneralBoard generalBoard, String path) {
-        _announcements = announcements;
-        _users = users;
-        _generalBoard = generalBoard;
-        this.path = path;
+        if (Files.isDirectory(Paths.get(path))) {
+            //Do something like throwing an exception
+        }
+
+        _file = new File(path);
+        if (!Files.exists(Paths.get(path))) {
+            //File does not exist
+            _file.createNewFile();
+            FileUtils.writeStringToFile(_file, "\"operation\" : []", StandardCharsets.UTF_8);
+        }
     }
 
     public void save() {
@@ -27,6 +43,6 @@ public class PersistenceManager {
     }
 
     public ServiceDPASImpl load() {
-
+        return null;
     }
 }
