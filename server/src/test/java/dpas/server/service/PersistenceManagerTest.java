b283d20213df3003848a653682d654cab586afe5
Miguel Barros <miguel.v.barros@tecnico.ulisboa.pt>
2020-03-18 16:55:16 +0000

    test load invalid file

--- server/src/test/java/dpas/server/service/PersistenceManagerTest.java
+++ server/src/test/java/dpas/server/service/PersistenceManagerTest.java
@@ -1,14 +1,20 @@
 package dpas.server.service;
 
+import dpas.common.domain.exception.*;
 import dpas.server.persistence.PersistenceManager;
 import org.apache.commons.io.FileUtils;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import javax.json.JsonException;
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SignatureException;
+import java.security.spec.InvalidKeySpecException;
 
 import static org.junit.Assert.assertEquals;
 
@@ -24,15 +30,12 @@ public class PersistenceManagerTest {
 
     }
 
-    @Test(expected = RuntimeException.class)
-    public void constructorFileIsDirectory() throws IOException {
-        new PersistenceManager("/tmp");
+    @Test(expected = JsonException.class)
+    public void loadInvalidFile() throws IOException, SignatureException, NullUserException, NullAnnouncementException, InvalidUserException, InvalidKeySpecException, NullMessageException, NoSuchAlgorithmException, InvalidMessageSizeException, NullPublicKeyException, InvalidKeyException, NullUsernameException, InvalidSignatureException, NullSignatureException, InvalidReferenceException {
+        ClassLoader classLoader = getClass().getClassLoader();
+        String path = classLoader.getResource("empty.json").getPath();;
+        PersistenceManager manager = new PersistenceManager(path);
+        manager.load();
     }
 
-    @Test
-    public void constructorFileDoesNotExist() throws IOException {
-        new PersistenceManager("/tmp/testFile_1");
-        String content = FileUtils.readFileToString(new File("/tmp/testFile_1"), StandardCharsets.UTF_8);
-        assertEquals(content, "\"operation\" : []");
-    }
 }
