84202a9b885e217b3afaa9f3189a81432b96312d
miguel <miguel.v.barros@tecnico.ulisboa.pt>
2020-03-29 17:39:29 +0100

    Cleanup

--- utils/src/main/java/dpas/utils/bytes/ContractUtils.java
+++ utils/src/main/java/dpas/utils/bytes/ContractUtils.java
@@ -2,17 +2,31 @@ package dpas.utils.bytes;
 
 import dpas.grpc.contract.Contract;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class ContractUtils {
-    public static byte[] toByteArray(Contract.SafePostRequest request) {
-        long seq = request.getSeq();
+    public static byte[] toByteArray(Contract.SafePostRequest request) throws IOException {
+        byte[] seq = LongUtils.longToBytes(request.getSeq());
         byte[] nonce = request.getSessionNonce().getBytes();
         byte[] pubKey = request.getPublicKey().toByteArray();
         byte[] message = request.getMessage().toByteArray();
         byte[] signature = request.getSignature().toByteArray();
-        List<byte[]> refs = request.getReferencesList().stream().map(String::getBytes).collect(Collectors.toList());
-        return null;
+        List<byte[]> references = request.getReferencesList().stream().map(String::getBytes).collect(Collectors.toList());
+        try (ByteArrayOutputStream stream = new ByteArrayOutputStream()) {
+            stream.writeBytes(seq);
+            stream.writeBytes(nonce);
+            stream.writeBytes(nonce);
+            stream.writeBytes(pubKey);
+            stream.writeBytes(message);
+            stream.writeBytes(signature);
+            stream.writeBytes(message);
+            for(var ref: references) {
+                stream.writeBytes(ref);
+            }
+            return stream.toByteArray();
+        }
     }
 }
