695649a669c91e0e747de11ed429c3539a7f0b9a
miguel <miguel.v.barros@tecnico.ulisboa.pt>
2020-04-10 10:31:41 +0100

    Cleanup

--- utils/src/main/java/dpas/utils/MacVerifier.java
+++ utils/src/main/java/dpas/utils/MacVerifier.java
@@ -126,4 +126,11 @@ public class MacVerifier {
         }
 
     }
+
+    public static boolean verifyMac(PublicKey serverPKey, Contract.MacReply reply, Contract.PostRequest request) throws GeneralSecurityException {
+        byte[] content = request.getMac().toByteArray();
+        byte[] mac = reply.getMac().toByteArray();
+        return verifyMac(serverPKey, content, mac);
+    }
+
 }
