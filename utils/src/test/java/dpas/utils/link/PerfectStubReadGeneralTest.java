9a21f2bae6e9de6b9f3a63a453073d6d1c07a12c
miguel <miguel.v.barros@tecnico.ulisboa.pt>
2020-04-18 17:44:49 +0100

    Read General

---
+++ utils/src/test/java/dpas/utils/link/PerfectStubReadGeneralTest.java
@@ -0,0 +1,246 @@
+package dpas.utils.link;
+
+import com.google.protobuf.ByteString;
+import dpas.grpc.contract.Contract;
+import dpas.grpc.contract.ServiceDPASGrpc;
+import dpas.utils.auth.ErrorGenerator;
+import dpas.utils.auth.MacGenerator;
+import io.grpc.Status;
+import io.grpc.inprocess.InProcessChannelBuilder;
+import io.grpc.inprocess.InProcessServerBuilder;
+import io.grpc.stub.StreamObserver;
+import io.grpc.testing.GrpcCleanupRule;
+import io.grpc.util.MutableHandlerRegistry;
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.security.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static io.grpc.Status.CANCELLED;
+import static io.grpc.Status.INVALID_ARGUMENT;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+public class PerfectStubReadGeneralTest {
+    @Rule
+    public final GrpcCleanupRule grpcCleanup = new GrpcCleanupRule();
+
+    private final MutableHandlerRegistry serviceRegistry = new MutableHandlerRegistry();
+
+    private static PublicKey _pubKey;
+    private static PrivateKey _privKey;
+    private static PrivateKey _serverPrivKey;
+    private static PublicKey _serverPKey;
+
+
+    private ServiceDPASGrpc.ServiceDPASStub client;
+
+
+    @BeforeClass
+    public static void oneTimeSetup() throws GeneralSecurityException {
+        KeyPairGenerator keygen = KeyPairGenerator.getInstance("RSA");
+        keygen.initialize(4096);
+
+        KeyPair serverPair = keygen.generateKeyPair();
+        _serverPKey = serverPair.getPublic();
+        _serverPrivKey = serverPair.getPrivate();
+
+        KeyPair keyPair = keygen.generateKeyPair();
+        _pubKey = keyPair.getPublic();
+        _privKey = keyPair.getPrivate();
+
+    }
+
+    @Test
+    public void readInvalidExceptions() throws IOException {
+        String serverName = InProcessServerBuilder.generateName();
+        grpcCleanup.register(InProcessServerBuilder.forName(serverName)
+                .fallbackHandlerRegistry(serviceRegistry).directExecutor().build().start());
+        client = ServiceDPASGrpc.newStub(grpcCleanup.register(
+                InProcessChannelBuilder.forName(serverName).directExecutor().build()));
+        PerfectStub pstub = new PerfectStub(client, _serverPKey);
+        final AtomicInteger countCompleted = new AtomicInteger(0);
+        final AtomicInteger countSuccess = new AtomicInteger(0);
+
+        ServiceDPASGrpc.ServiceDPASImplBase impl = new  ServiceDPASGrpc.ServiceDPASImplBase() {
+            AtomicInteger i = new AtomicInteger(3);
+
+            @Override
+            public void readGeneral(Contract.ReadRequest request, StreamObserver<Contract.ReadReply> responseObserver) {
+                try {
+                    int j = i.getAndDecrement();
+                    if (j == 0) {
+                        List<Contract.Announcement> announcements = new ArrayList<>();
+                        responseObserver.onNext(Contract.ReadReply.newBuilder()
+                                .addAllAnnouncements(announcements)
+                                .setMac(ByteString.copyFrom(MacGenerator.generateMac(request, announcements.size(), _serverPrivKey)))
+                                .build());
+                        responseObserver.onCompleted();
+                        return;
+                    }
+                    responseObserver.onError(Status.UNKNOWN.asRuntimeException());
+
+                } catch (GeneralSecurityException | IOException e) {
+                    fail();
+                }
+            }
+        };
+        serviceRegistry.addService(impl);
+        CountDownLatch latch = new CountDownLatch(1);
+        pstub.readGeneral(Contract.ReadRequest.newBuilder()
+                .setPublicKey(ByteString.copyFrom(_pubKey.getEncoded()))
+                .setNumber(2)
+                .build(), new StreamObserver<>() {
+            @Override
+            public void onNext(Contract.ReadReply value) {
+                countSuccess.getAndIncrement();
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                fail();
+            }
+
+            @Override
+            public void onCompleted() {
+                countCompleted.getAndIncrement();
+                latch.countDown();
+            }
+        });
+
+        try {
+            if (!latch.await(4000, TimeUnit.SECONDS)) {
+                fail();
+            }
+            assertEquals(countSuccess.get(), 1);
+            assertEquals(countCompleted.get(), 1);
+        } catch (InterruptedException e) {
+            fail();
+        }
+    }
+
+
+    @Test
+    public void readValidExceptions() throws IOException {
+        String serverName = InProcessServerBuilder.generateName();
+        grpcCleanup.register(InProcessServerBuilder.forName(serverName)
+                .fallbackHandlerRegistry(serviceRegistry).directExecutor().build().start());
+        client = ServiceDPASGrpc.newStub(grpcCleanup.register(
+                InProcessChannelBuilder.forName(serverName).directExecutor().build()));
+        PerfectStub pstub = new PerfectStub(client, _serverPKey);
+        final AtomicInteger countCompleted = new AtomicInteger(0);
+        final AtomicInteger countSuccess = new AtomicInteger(0);
+        ServiceDPASGrpc.ServiceDPASImplBase impl = new  ServiceDPASGrpc.ServiceDPASImplBase() {
+            AtomicInteger i = new AtomicInteger(3);
+            @Override
+            public void readGeneral(Contract.ReadRequest request, StreamObserver<Contract.ReadReply> responseObserver) {
+                try {
+                    int j = i.getAndDecrement();
+                    if (j == 0) {
+                        List<Contract.Announcement> announcements = new ArrayList<Contract.Announcement>();
+                        responseObserver.onNext(Contract.ReadReply.newBuilder()
+                                .addAllAnnouncements(announcements)
+                                .setMac(ByteString.copyFrom(MacGenerator.generateMac(request, announcements.size(), _serverPrivKey)))
+                                .build());
+                        responseObserver.onCompleted();
+                        return;
+                    }
+                    responseObserver.onError(ErrorGenerator.generate(CANCELLED, "Invalid security values provided", request, _serverPrivKey));
+
+                } catch (GeneralSecurityException | IOException e) {
+                    fail();
+                }
+            }
+        };
+        serviceRegistry.addService(impl);
+        CountDownLatch latch = new CountDownLatch(1);
+        pstub.readGeneral(Contract.ReadRequest.newBuilder()
+                .setPublicKey(ByteString.copyFrom(_pubKey.getEncoded()))
+                .setNumber(2)
+                .build(), new StreamObserver<>() {
+            @Override
+            public void onNext(Contract.ReadReply value) {
+                countSuccess.getAndIncrement();
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                fail();
+            }
+
+            @Override
+            public void onCompleted() {
+                countCompleted.getAndIncrement();
+                latch.countDown();
+            }
+        });
+
+        try {
+            if (!latch.await(4000, TimeUnit.SECONDS)) {
+                fail();
+            }
+        } catch (InterruptedException e) {
+            fail();
+        }
+    }
+
+
+    @Test
+    public void readNoExceptionsImmediateReply() throws IOException {
+        String serverName = InProcessServerBuilder.generateName();
+        grpcCleanup.register(InProcessServerBuilder.forName(serverName)
+                .fallbackHandlerRegistry(serviceRegistry).directExecutor().build().start());
+        client = ServiceDPASGrpc.newStub(grpcCleanup.register(
+                InProcessChannelBuilder.forName(serverName).directExecutor().build()));
+        PerfectStub pstub = new PerfectStub(client, _serverPKey);
+        final AtomicInteger countCompleted = new AtomicInteger(0);
+        final AtomicInteger countSuccess = new AtomicInteger(0);
+        ServiceDPASGrpc.ServiceDPASImplBase impl = new  ServiceDPASGrpc.ServiceDPASImplBase() {
+            @Override
+            public void readGeneral(Contract.ReadRequest request, StreamObserver<Contract.ReadReply> responseObserver) {
+                try {
+                    List<Contract.Announcement> announcements = new ArrayList<>();
+                    responseObserver.onNext(Contract.ReadReply.newBuilder()
+                            .addAllAnnouncements(announcements)
+                            .setMac(ByteString.copyFrom(MacGenerator.generateMac(request, announcements.size(), _serverPrivKey)))
+                            .build());
+                    responseObserver.onCompleted();
+
+                } catch (GeneralSecurityException | IOException e) {
+                    responseObserver.onError(ErrorGenerator.generate(INVALID_ARGUMENT, e.getMessage(), request, _privKey));
+                }
+            }
+        };
+        serviceRegistry.addService(impl);
+        CountDownLatch latch = new CountDownLatch(1);
+        pstub.readGeneral(Contract.ReadRequest.newBuilder()
+                .setPublicKey(ByteString.copyFrom(_pubKey.getEncoded()))
+                .setNumber(2)
+                .build(), new StreamObserver<>() {
+            @Override
+            public void onNext(Contract.ReadReply value) {
+                countSuccess.getAndIncrement();
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                fail();
+            }
+
+            @Override
+            public void onCompleted() {
+                countCompleted.getAndIncrement();
+                latch.countDown();
+            }
+        });
+    }
+
+
+}
