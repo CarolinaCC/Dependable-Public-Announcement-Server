debdd09bac48728fa4d8e2961a35b4e5634ab657
CatarinaPedreira <catarina.pedreira@tecnico.ulisboa.pt>
2020-04-18 14:26:42 +0100

    read with exception

--- utils/src/main/java/dpas/utils/link/PerfectStub.java
+++ utils/src/main/java/dpas/utils/link/PerfectStub.java
@@ -1,11 +1,14 @@
 package dpas.utils.link;
 
+import com.google.protobuf.ByteString;
 import dpas.grpc.contract.Contract;
 import dpas.grpc.contract.ServiceDPASGrpc;
+import dpas.utils.auth.CipherUtils;
 import dpas.utils.auth.MacVerifier;
 import dpas.utils.auth.ReplyValidator;
 import io.grpc.stub.StreamObserver;
 
+import java.security.GeneralSecurityException;
 import java.security.PublicKey;
 
 /**
@@ -102,6 +105,43 @@ public class PerfectStub {
                     //Response was not authenticated, so It must be the attacker or a byzantine server
                     //Either way retry until obtaining a valid answer
                     readGeneralWithException(request, replyObserver);
+                } else {
+                    replyObserver.onError(t);
+                }
+            }
+
+            @Override
+            public void onCompleted() {
+                replyObserver.onCompleted();
+            }
+        });
+    }
+
+
+    public void readWithException(Contract.ReadRequest request, StreamObserver<Contract.ReadReply> replyObserver) {
+        _stub.read(request, new StreamObserver<>() {
+            @Override
+            public void onNext(Contract.ReadReply value) {
+                //If we can't verify the response then either the attacker changed it (must retry until he stops)
+                //Or the server is byzantine (since we can't know must keep trying)
+                //Since the operation is idempotent resending to a correct server has no impact
+                try {
+                    if (!ReplyValidator.validateReadReply(request, value, _serverKey, CipherUtils.keyFromBytes(request.getPublicKey().toByteArray()))) {
+                        readWithException(request, replyObserver);
+                    } else {
+                        replyObserver.onNext(value);
+                    }
+                } catch (GeneralSecurityException e) {
+                    readWithException(request, replyObserver);
+                }
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                if (!ReplyValidator.verifyError(t, request, _serverKey)) {
+                    //Response was not authenticated, so It must be the attacker or a byzantine server
+                    //Either way retry until obtaining a valid answer
+                    readWithException(request, replyObserver);
                 } else {
                     replyObserver.onError(t);
                 }
