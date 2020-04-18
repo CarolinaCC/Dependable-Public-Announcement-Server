28ea6b78192817d8ecaee78ada5c5e66d365016d
miguel <miguel.v.barros@tecnico.ulisboa.pt>
2020-04-18 14:15:55 +0100

    Perfect stub

--- utils/src/main/java/dpas/utils/link/PerfectStub.java
+++ utils/src/main/java/dpas/utils/link/PerfectStub.java
@@ -114,4 +114,34 @@ public class PerfectStub {
         });
     }
 
+    public void readGeneral(Contract.ReadRequest request, StreamObserver<Contract.ReadReply> replyObserver) {
+        _stub.readGeneral(request, new StreamObserver<>() {
+            @Override
+            public void onNext(Contract.ReadReply value) {
+                //If we can't verify the response then either the attacker changed it (must retry until he stops)
+                //Or the server is byzantine (since we can't know must keep trying)
+                //Since the operation is idempotent resending to a correct server has no impact
+                if (!MacVerifier.verifyMac(request, value, _serverKey)) {
+                    readGeneralWithException(request, replyObserver);
+                } else {
+                    replyObserver.onNext(value);
+                }
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                //If an error occurred it is either a byzantine client (we don't care about him)
+                //The attacker changed the integrity parameters (we must keep trying until the attacker gives up)
+                //A byzantine server (since we can't know, we must retry still)
+                //Some previous post this depends on or a register hasn't reached the server, we must also retry until it does
+                replyObserver.onError(t);
+            }
+
+            @Override
+            public void onCompleted() {
+                replyObserver.onCompleted();
+            }
+        });
+    }
+
 }
