39dffbc27d3f1b286da98e1e7355f85f1136a81d
miguel <miguel.v.barros@tecnico.ulisboa.pt>
2020-04-18 16:04:55 +0100

    Perfect Stub Fix

--- utils/src/main/java/dpas/utils/link/PerfectStub.java
+++ utils/src/main/java/dpas/utils/link/PerfectStub.java
@@ -201,7 +201,7 @@ public class PerfectStub {
                 //The attacker changed the integrity parameters (we must keep trying until the attacker gives up)
                 //A byzantine server (since we can't know, we must retry still)
                 //Some previous post this depends on or a register hasn't reached the server, we must also retry until it does
-                replyObserver.onError(t);
+                readGeneral(request, replyObserver);
             }
 
             @Override
