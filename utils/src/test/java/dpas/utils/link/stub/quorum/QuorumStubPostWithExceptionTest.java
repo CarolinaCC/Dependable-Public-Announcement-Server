8aa005edae8727b40adeb0a3e9125c203e75afb9
miguel <miguel.v.barros@tecnico.ulisboa.pt>
2020-04-19 10:08:39 +0100

    Eventual Exception Consensus

--- utils/src/test/java/dpas/utils/link/stub/quorum/QuorumStubPostWithExceptionTest.java
+++ utils/src/test/java/dpas/utils/link/stub/quorum/QuorumStubPostWithExceptionTest.java
@@ -1,13 +1,11 @@
 package dpas.utils.link.stub.quorum;
 
-import com.google.protobuf.ByteString;
 import dpas.common.domain.exception.CommonDomainException;
 import dpas.grpc.contract.Contract;
 import dpas.grpc.contract.ServiceDPASGrpc;
 import dpas.utils.ContractGenerator;
 import dpas.utils.auth.CipherUtils;
 import dpas.utils.auth.ErrorGenerator;
-import dpas.utils.auth.MacGenerator;
 import dpas.utils.link.PerfectStub;
 import dpas.utils.link.QuorumStub;
 import io.grpc.inprocess.InProcessChannelBuilder;
@@ -20,12 +18,10 @@ import org.junit.Rule;
 import org.junit.Test;
 
 import java.io.IOException;
-import java.lang.reflect.Array;
-import java.nio.charset.StandardCharsets;
 import java.security.*;
 import java.util.ArrayList;
-import java.util.Dictionary;
 import java.util.List;
+import java.util.UUID;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import static io.grpc.Status.CANCELLED;
@@ -90,7 +86,7 @@ public class QuorumStubPostWithExceptionTest {
         var qstub = new QuorumStub(stubs, 1);
 
         qstub.postWithException(_request);
-        for(int number: _assertions) {
+        for (int number : _assertions) {
             assertEquals(number, 1);
         }
         assertEquals(_assertions.size(), 4);
@@ -118,7 +114,7 @@ public class QuorumStubPostWithExceptionTest {
         var qstub = new QuorumStub(stubs, 1);
 
         qstub.postWithException(_request);
-        for(int number: _assertions) {
+        for (int number : _assertions) {
             assertEquals(number, 1);
         }
         assertEquals(_assertions.size(), 4);
@@ -146,7 +142,7 @@ public class QuorumStubPostWithExceptionTest {
         var qstub = new QuorumStub(stubs, 1);
 
         qstub.postWithException(_request);
-        for(int number: _assertions) {
+        for (int number : _assertions) {
             assertEquals(number, 1);
         }
         assertEquals(_assertions.size(), 12);
@@ -175,11 +171,44 @@ public class QuorumStubPostWithExceptionTest {
 
         try {
             qstub.postWithException(_request);
-        }catch(RuntimeException e) {
+        } catch (RuntimeException e) {
             for (int number : _assertions) {
                 assertEquals(number, 1);
             }
             assertEquals(_assertions.size(), 4);
+            assertEquals(e.getMessage(), "CANCELLED: Invalid security values provided");
+            throw e;
+        }
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void eventualExceptionConsensus() throws IOException, InterruptedException, GeneralSecurityException {
+        var servers = allExceptionsDifferentThenEqual();
+        var stubs = new ArrayList<PerfectStub>();
+        int i = 0;
+        for (var server : servers) {
+            serviceRegistry[i] = new MutableHandlerRegistry();
+            var registry = serviceRegistry[i];
+            String serverName = InProcessServerBuilder.generateName();
+            grpcCleanup.register(InProcessServerBuilder.forName(serverName)
+                    .fallbackHandlerRegistry(registry).directExecutor().build().start());
+            registry.addService(server);
+            ServiceDPASGrpc.ServiceDPASStub client = ServiceDPASGrpc.newStub(grpcCleanup.register(
+                    InProcessChannelBuilder.forName(serverName).directExecutor().build()));
+            PerfectStub pstub = new PerfectStub(client, _serverPKey[i]);
+            stubs.add(pstub);
+            i++;
+        }
+
+        var qstub = new QuorumStub(stubs, 1);
+
+        try {
+            qstub.postWithException(_request);
+        } catch (RuntimeException e) {
+            for (int number : _assertions) {
+                assertEquals(number, 1);
+            }
+            assertEquals(_assertions.size(), 12);
             assertEquals(e.getMessage(), "CANCELLED: Invalid security values provided");
             throw e;
         }
@@ -266,6 +295,31 @@ public class QuorumStubPostWithExceptionTest {
     }
 
 
+
+    public static List<ServiceDPASGrpc.ServiceDPASImplBase> allExceptionsDifferentThenEqual() {
+        List<ServiceDPASGrpc.ServiceDPASImplBase> servers = new ArrayList<>();
+        for (int i = 0; i < 4; i++) {
+            final int j = i;
+            AtomicInteger t = new AtomicInteger(j);
+            servers.add(
+                    new ServiceDPASGrpc.ServiceDPASImplBase() {
+                        @Override
+                        public void post(Contract.Announcement request, StreamObserver<Contract.MacReply> responseObserver) {
+                            _assertions.add(1);
+
+                            int k = t.getAndDecrement();
+                            if (k <= 0) {
+                                responseObserver.onError(ErrorGenerator.generate(CANCELLED, "Invalid security values provided", request, _serverPrivKey[j]));
+                            } else {
+                                responseObserver.onError(ErrorGenerator.generate(CANCELLED, UUID.randomUUID().toString(), request, _serverPrivKey[j]));
+                            }
+                        }
+                    });
+        }
+        return servers;
+    }
+
+
     public static List<ServiceDPASGrpc.ServiceDPASImplBase> allEmpyServersTwoExceptionsThenSuccess() {
         List<ServiceDPASGrpc.ServiceDPASImplBase> servers = new ArrayList<>();
         for (int i = 0; i < 2; i++) {
@@ -290,6 +344,7 @@ public class QuorumStubPostWithExceptionTest {
             servers.add(
                     new ServiceDPASGrpc.ServiceDPASImplBase() {
                         AtomicInteger t = new AtomicInteger(2);
+
                         @Override
                         public void post(Contract.Announcement request, StreamObserver<Contract.MacReply> responseObserver) {
                             if (j == 3) {
@@ -305,8 +360,8 @@ public class QuorumStubPostWithExceptionTest {
                                     return;
                                 }
                             }
-                                _assertions.add(1);
-                                responseObserver.onError(ErrorGenerator.generate(CANCELLED, "Invalid security values provided", request, _serverPrivKey[j]));
+                            _assertions.add(1);
+                            responseObserver.onError(ErrorGenerator.generate(CANCELLED, "Invalid security values provided", request, _serverPrivKey[j]));
 
 
                         }
